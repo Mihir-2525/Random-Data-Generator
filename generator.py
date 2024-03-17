08a2c4f43f2a0e1ae21796abb144df54f9192638
Mihir Rathod <113364270+Mihir-2525@users.noreply.github.com>
2024-03-17 18:42:31 +0530

    first commit

---
+++ generator.py
@@ -0,0 +1,55 @@
+import random
+import csv
+from datetime import datetime, timedelta
+
+def generate_random_birthdate(start_year=1980, end_year=2005):
+    start_date = datetime(start_year, 1, 1)
+    end_date = datetime(end_year, 12, 31)
+    delta = end_date - start_date
+    random_days = random.randint(0, delta.days)
+    random_date = start_date + timedelta(days=random_days)
+    return random_date.strftime("%Y-%m-%d")
+
+def data():
+    # Generate mobile number
+    mobile_number = int(f"{random.choice([6, 8, 9])}{random.randint(100000000, 999999999)}")
+    
+    # Generate gender
+    gender = random.choice(['male', 'female'])
+    
+    # Generate name and surname based on gender
+    male_names = ['Aarav', 'Amit', 'Arjun', 'Ishaan', 'Rahul', 'Varun', 'Yash', 'Amitabh', 'Kunal', 'Pranav', 'Vivek', 'Aryan', 'Hritik', 'Manish', 'Siddharth', 'Akash', 'Gaurav', 'Mohan', 'Parth', 'Ramesh', 'Vijay', 'Dinesh', 'Gopal', 'Prashant', 'Ravi', 'Sandeep', 'Vikram', 'Deepak', 'Harish', 'Rohan', 'Rajesh', 'Vishal', 'Shivam', 'Vijay', 'Shubham', 'Aakash', 'Raj', 'Arun', 'Rajat', 'Karan', 'Rajesh', 'Rishi', 'Rajiv', 'Abhinav', 'Rahul', 'Kamal', 'Vinay', 'Suresh', 'Sanjay']
+    female_names = ['Ananya', 'Divya', 'Kavya', 'Sarika', 'Tanvi', 'Aditi', 'Anjali', 'Maya', 'Shreya', 'Zoya', 'Deepika', 'Kritika', 'Pooja', 'Riya', 'Chandni', 'Ganesh', 'Kamala', 'Manisha', 'Nisha', 'Uma', 'Yogita', 'Bhavna', 'Hema', 'Jyoti', 'Kiran', 'Neeta', 'Suman', 'Yogesh', 'Akanksha', 'Ishita', 'Shalini', 'Swati', 'Sunita', 'Preeti', 'Neelam', 'Anita', 'Ritu', 'Rashmi', 'Sangeeta', 'Pallavi','Rajani', 'Mamta', 'Indira', 'Lakshmi', 'Radha', 'Savita', 'Geeta', 'Nandini', 'Kiran', 'Vandana']
+    surnames = ['Patel', 'Sharma', 'Gupta', 'Kumar', 'Singh', 'Das', 'Patil', 'Chatterjee', 'Reddy', 'Naidu', 'Mukherjee', 'Rao', 'Joshi', 'Desai', 'Banerjee', 'Mishra', 'Malhotra', 'Mehta', 'Sinha', 'Saxena', 'Verma','Pandey', 'Shah', 'Thakur', 'Jain', 'Srivastava', 'Choudhury', 'Chauhan', 'Bhat', 'Seth', 'Pillai','Acharya', 'Agarwal', 'Biswas', 'Chopra', 'Datta', 'Dubey', 'Ganguly', 'Ghosh', 'Kapoor', 'Khanna','Lal', 'Mahajan', 'Nair', 'Panicker', 'Raman', 'Sen', 'Tiwari', 'Trivedi', 'Varghese', 'Yadav','Shroff', 'Parekh', 'Jha', 'Choudhary', 'Dasgupta', 'Shinde', 'Pawar', 'Goswami', 'Nanda', 'Sengupta', 'Rathod', 'Prajapati', 'Chauhan', 'Solanki', 'Rana', 'Thakkar', 'Vaghela', 'Doshi', 'Bhattacharya', 'Meena']
+    name, surname = random.choice(male_names) if gender == 'male' else random.choice(female_names), random.choice(surnames)
+    
+    # Generate email
+    email = f"{name.lower()}{surname.lower()}{random.choice([i+1 for i in range(30)])}@gmail.com"
+    
+    return {'Name': name, 'Surname': surname, 'Gender': gender, 'Email': email, 'Mobile Number': mobile_number, 'Birthdate': generate_random_birthdate()}
+
+def random_data_to_sql(numbers):
+    with open("random_generated_data.sql", 'w') as sql:
+        sql.write(f"INSERT INTO `table_name` (`name`, `surname`, `gender`, `mobile_number`, `email`, `birthdate`) \nVALUES \n")
+        for _ in range(numbers):
+            # Get random data
+            row_data = data()
+            # Generate SQL INSERT statement
+            sql.write(f"\t('{row_data['Name']}', '{row_data['Surname']}', '{row_data['Gender']}', {row_data['Mobile Number']}, '{row_data['Email']}', '{row_data['Birthdate']}')")
+            sql.write(";") if (_+1 == numbers) else sql.write(",\n")
+
+def random_data_to_csv(numbers):
+    with open('random_generated_data.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Name', 'Surname', 'Gender', 'Mobile Number', 'Email', 'Birthdate'])
+        for _ in range(numbers):
+            # Get random data
+            row_data = data()
+            # Write data to CSV file
+            writer.writerow([row_data['Name'], row_data['Surname'], row_data['Gender'], row_data['Mobile Number'], row_data['Email'], row_data['Birthdate']])
+
+# Generate SQL INSERT statements 
+random_data_to_sql(25)
+
+# Generate random entries and write them to a CSV file
+random_data_to_csv(25)
